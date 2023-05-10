ceae881193a86d5e273a63ae09d1c660718bed1c
DEV-ADASTRA <dev@adastra-commercial.mx>
2023-05-09 18:10:42 -0600

    UPTDATE

--- bd/login.php
+++ bd/login.php
@@ -9,7 +9,9 @@ $conexion = $objeto->Conectar();
 $usuario = (isset($_POST['usuario'])) ? $_POST['usuario'] : '';
 $password = (isset($_POST['password'])) ? $_POST['password'] : '';
 
-$pass = md5($password); //encripto la clave enviada por el usuario para compararla con la clava encriptada y almacenada en la BD
+$pass = md5($password); 
+
+//encripto la clave enviada por el usuario para compararla con la clava encriptada y almacenada en la BD
 
 $consulta = "SELECT * FROM usuarios WHERE usuario='$usuario' AND password='$pass' ";
 $resultado = $conexion->prepare($consulta);
